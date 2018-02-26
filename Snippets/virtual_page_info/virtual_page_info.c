9dae36b5a86b6a3f5a728be482019791fc7490f5
unknown <timothy.z.gan@gmail.com>
2018-02-27 02:40:20 +0800

    Fix typo

--- Snippets/virtual_page_info/virtual_page_info.c
+++ Snippets/virtual_page_info/virtual_page_info.c
@@ -225,7 +225,7 @@ int main(int argc, char *argv[]){
     return 3;
   }
   
-  // Print the hexadecimal data in the memory address
+  // Print data on the memory pages in the requested process
   mapVirtualPages(processId);
   
   return 0;
